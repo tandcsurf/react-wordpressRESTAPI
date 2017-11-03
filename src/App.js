4b34d9d009988e5320e381b372f959db52c962f4
Lawrence Stiers <lawrencestiers@mbp.local>
2017-11-03 15:25:06 -0400

    got post titles and content rendered out

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      posts: [],
+    };
+  }
+
+  renderPosts(post) {
+    return <div key={post.id}>
+    <h2 dangerouslySetInnerHTML={{__html: post.title.rendered}} />
+    <p dangerouslySetInnerHTML={{__html: post.content.rendered}} />
+    </div>
+  }
+
+  componentDidMount() {
+    const headers = new Headers({
+      'Content-Type': 'application/json',
+    });
+    const init = {
+      method: 'GET',
+      headers,
+      mode: 'cors',
+    }
+    fetch("http://li468-9.members.linode.com/wp-json/wp/v2/posts", init)
+      .then(response => response.json())
+      .then(parsedResponse => this.setState({posts: parsedResponse}));
+  }
   render() {
+    console.log(this.state.posts, "this.state.posts");
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {this.state.posts.map(this.renderPosts)}
       </div>
     );
   }
